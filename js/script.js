57dfc58e14a3654c4904ca769e946a046b1bfecb
Lil Kitchen <lil.kitchen@hotmail.com>
2022-02-21 14:18:30 +0500

    Good AI with a flaw

--- js/script.js
+++ js/script.js
@@ -5,10 +5,10 @@ const MODE_2P = '2 Players';
 const MODE_AI = 'AI';
 const MODE_NINNY = 'Ninny';
 
-const X_MARK = "X"
-const O_MARK = "O"
+const X_MARK = "X";
+const O_MARK = "O";
 
-let move = 0;
+let mark = X_MARK;
 let mode = MODE_2P;
 button_change.innerHTML = mode;
 
@@ -21,10 +21,18 @@ const btnClose = document.getElementById('btn-close');
 function get_boxes()
 {
 	let boxes = [9];
+	let i = 0;
 	for (i = 0; i < 9; i++) {
 		boxes[i] = document.getElementById('box' + i).innerHTML;
 	}
-	return boxes
+	return boxes;
+}
+
+function reload()
+{
+	mark = X_MARK;
+	for (i = 0; i < 9; i++)
+		document.getElementById('box' + i).innerHTML = '';
 }
 
 button_change.addEventListener('click', e=>{
@@ -40,38 +48,73 @@ button_change.addEventListener('click', e=>{
 		break;
 	}
 	e.target.innerHTML = mode;
-	move = 0;
-	for (i = 0; i < 9; i++) {
-		document.getElementById('box' + i).innerHTML = '';
-	}
+	reload();
 });
 
 area.addEventListener('click', e=>{
 	if (e.target.className = 'box') {
-		if (mode == MODE_2P) 
-			mark = move % 2 == 0 ? X_MARK : O_MARK;
-		else
-			mark = X_MARK;
 		if (e.target.innerHTML == "") {
 			e.target.innerHTML = mark;
 			if (check())
-				ai();
+				if (mode != MODE_2P)
+					ai();
 		}
 	}
 });
 
+function get_empty(boxes)
+{
+	let empty = [];
+	let i = 0;
+	for (i in boxes)
+		if (boxes[i] == "")
+			empty.push(i);
+	return empty;
+}
+
+function mark_reverse(mark)
+{
+	if (mark == X_MARK)
+		return O_MARK;
+	return X_MARK;
+}
+
+function get_cell_score(boxes, cell, mrk, winmrk)
+{
+	let score = 0;
+	let next_boxes = boxes.slice();
+	next_boxes[cell] = mrk;
+	let next_mrk = mark_reverse(mrk);
+	switch (check_win(next_boxes)) {
+		case winmrk: return 1;
+		case mark_reverse(winmrk): return -1;
+		case 1: return 0;
+	}
+	let next_empty = get_empty(next_boxes);
+	let i = 0;
+	for (i in next_empty) {
+		score += get_cell_score(next_boxes, next_empty[i], next_mrk, winmrk);
+	}
+	return score;
+}
+
 function ai()
 {
-	let boxes = get_boxes();
-	let empty = [];
 	let choice;
+	let boxes = get_boxes();
+	let empty = get_empty(boxes);
 	if (mode == MODE_AI) {
-		;
-	} else if (mode == MODE_NINNY) {
-		for (i = 0; i < 9; i++) {
-			if (boxes[i] == "")
-				empty.push(i);
+		let highest = -100000;
+		let sc;
+		let k = 0;
+		for (k in empty) {
+			sc = get_cell_score(boxes, empty[k], mark, O_MARK);
+			if (sc > highest) {
+				choice = empty[k];
+				highest = sc;
+			}
 		}
+	} else if (mode == MODE_NINNY) {
 		choice = empty[Math.round(Math.random() * (empty.length - 1))];
 	}
 	target = document.getElementById('box' + choice);
@@ -79,11 +122,9 @@ function ai()
 	check();
 }
 
-function check()
+function check_win(boxes)
 {
-	move++;
-	let boxes = get_boxes();
-	const arr = [
+	const lines = [
 		[0,1,2],
 		[3,4,5],
 		[6,7,8],
@@ -91,28 +132,43 @@ function check()
 		[1,4,7],
 		[2,5,8],
 		[0,4,8],
-		[2,4,6]
-	];
-	for (i = 0; i < arr.length; i++) {
-		if (boxes[arr[i][0]] == X_MARK &&
-		    boxes[arr[i][1]] == X_MARK &&
-		    boxes[arr[i][2]] == X_MARK) {
-			result = 'кресты';
-			prepareResult(result);
-			return 0
-		} else if (boxes[arr[i][0]] == O_MARK &&
-			   boxes[arr[i][1]] == O_MARK &&
-			   boxes[arr[i][2]] == O_MARK) {
-			result = 'нули';
-			prepareResult(result);
-			return 0
+		[2,4,6]];
+	let i = 0;
+	for (i in lines) {
+		if (boxes[lines[i][0]] == X_MARK &&
+		    boxes[lines[i][1]] == X_MARK &&
+		    boxes[lines[i][2]] == X_MARK) {
+			return X_MARK;
+		} else if (boxes[lines[i][0]] == O_MARK &&
+			   boxes[lines[i][1]] == O_MARK &&
+			   boxes[lines[i][2]] == O_MARK) {
+			return O_MARK;
 		}
 	}
-	if (move == 9) {
-	    prepareResult(0);
-	    return 0
+	if (get_empty(boxes).length == 0) {
+		return 1;
 	}
-	return 1
+	return 0;
+}
+
+function check()
+{
+	mark = mark_reverse(mark);
+	switch (check_win(get_boxes())) {
+	case X_MARK:
+		result = 'кресты';
+		prepareResult(result);
+		return 0;
+		break;
+	case O_MARK:
+		result = 'нули';
+		prepareResult(result);
+		return 0;
+	case 1:
+		prepareResult(0);
+		return 0;
+	}
+	return 1;
 }
 const prepareResult = winner => {
 	let message = `Затащили ${winner}!`;
@@ -124,7 +180,7 @@ const prepareResult = winner => {
 
 const closeModal = () => {
 	modalResult.style.display = 'none';
-	location.reload();
+	reload();
 }
 
 overlay.addEventListener('click', closeModal);
